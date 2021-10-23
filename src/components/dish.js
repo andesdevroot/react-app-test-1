b89c1836b8b91ec613791a206e4ba97184345b8a
andesdevroot <andes.dev.root@gmail.com>
2021-10-22 23:28:39 -0300

    se crea otra clase ingredients

--- src/components/dish.js
+++ src/components/dish.js
@@ -7,18 +7,32 @@ export class Flag extends Component {
         <div>
             <h1>Bandera</h1>
         </div>
-        )
+        );
     }
 }
+
+export class Ingredient extends Component {
+    render () {
+        return (
+        <div>
+            <h1>Ingredientes</h1>
+        </div>
+        );
+    }
+}
+
 
 
 
 class Dish extends Component {
   render() {
     return (
+        <div>
         <h1> Platillo</h1>
-    )
- }
+        <Ingredient/>
+        </div>
+    );
+  }
 }
 
 export default Dish;
